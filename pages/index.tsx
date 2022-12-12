19df97cb999942824ea71923664858f43054953a
Bumskee <fadhlanrazan@gmail.com>
2022-12-13 02:11:33 +0700

    prepare for homepage works
    1. made the project card to be a component for easy import
    2. add the find all function to the project service.tsx
    3. removed unnecessary code from the profile.jsx since the projectCard is already a component.
    4. added the table for showcasing all the projects in the index.jss and prepare some methods to be easily copy pasted later for getting datas of the projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,49 @@
 import type { NextPage } from "next"
-import Head from 'next/head';
-import Imag from 'next/image';
+import { find_all_projects, ProjectData } from "../clients/project_service";
 import styles from '../styles/home.module.scss';
 import Navbar from "../components/navbar";
+import style from "../styles/profile.module.scss";
+import { useEffect, useState } from "react";
+import ProjectCard from "../components/projectCard";
 
 const Home: NextPage = () => {
+
+  const [userProjects, setUserProjects] = useState<ProjectData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const projectToProjectCard = (projectData: ProjectData) => (<ProjectCard key={projectData.project_id} projectData={projectData}/>)
+
+  useEffect(() => {
+    
+
+    const run = async () => {
+      setIsLoading(_ => true);
+      const allProjs = await find_all_projects();
+      if (allProjs.projects !== null)
+      {
+        const projs = allProjs.projects;
+        setUserProjects(_ => projs);
+      }
+      setIsLoading(_ => false);
+  };
+  run();
+  }, []);
+
     return (
     <div>
       <Navbar isLoading={false}/>
       <main className={styles.helloworld}>
-        This page is still a work in progress...
+        All of the projects, hopefully....
       </main>
+      <div>
+        <div className={style.separator}/>
+          <div className={style.project_card_container}>
+              {
+                  userProjects?.map(projectToProjectCard)
+              }
+          </div>
+      </div>
     </div>
+    
     )
 }
 export default Home;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "../components/navbar";
+import ProjectCard from "../components/projectCard"
 import style from "../styles/profile.module.scss";
 import profile from "../public/profile.jpg";
 import noimage from "../public/no-image-icon-6.png";
@@ -73,41 +74,6 @@ function ProfileDescription(profileDescriptionProps: ProfileDescriptionProps) {
 
 }
 
-type ProjectCardProps = {
-    projectData: ProjectData
-}
-
-function ProjectCard({ projectData }: ProjectCardProps) {
-    const router = useRouter();
-    let poster: StaticImageData | string = noimage;
-    if (projectData.poster_image.name !== "") {
-        poster = projectData.poster_image.base64;
-    }
-    return (
-        <>
-            <div className={style.project_card}>
-                <div className={style.project_poster}>
-                    <Image style={{width: "100%", height: "100%", position: "relative"}} src={poster} layout="fill" objectFit="cover" alt="poster"/>
-                </div>
-                <div className={style.project_description}>
-                    <h1>
-                        {projectData.name ? projectData.name : "No Name"}
-                    </h1>
-                    <p>
-                        {projectData.short_description}
-                    </p>
-                <Button
-                    variant="outlined" 
-                    color="info" 
-                    className={style.margin20px} onClick = {_ => {
-                        router.push(`/project/${projectData.project_id}`)
-                    }}>Read More</Button>
-                </div>
-            </div>
-        </>
-    )
-}
-
 function CreateProjectCard() {
     return (<>
         <Link href="/poster/create">
@@ -130,7 +96,6 @@ export default function Profile(profileProps: ProfileProps) {
     const [userData, setUserData] = useState<UserData>(EMPTY_USER_DATA);
     const [userProjects, setUserProjects] = useState<ProjectData[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
-
 
     useEffect(() => {
         const run = async () => {
