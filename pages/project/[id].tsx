56da851211687000c64a46c7f49548c0033dd000
Sleepy <sensen.edsel@gmail.com>
2022-12-15 23:33:27 +0700

    Comment
    add comment section n upload button

--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,134 +1,209 @@
-import Navbar from "../../components/navbar"
-import Footer from "../../components/footer"
-import Image from 'next/image'
-import style from "../../styles/project.id.module.scss"
-import { useEffect, useState } from "react"
-import { find_project_by_project_id,  ProjectData} from "../../clients/project_service"
-import { useRouter } from "next/router"
-import { FindUserByIdResponse, findUserById } from "../../clients/user_service"
-import youtube from '../../public/Project/youtube.png'
-import github from '../../public/Project/github.png'
-import website from '../../public/Project/web.png'
-import YouTube, { YouTubeProps } from 'react-youtube'
-import dynamic from "next/dynamic"
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
+import Image from "next/image";
+import style from "../../styles/project.id.module.scss";
+import { useEffect, useState } from "react";
+import { find_project_by_project_id } from "../../clients/project_service";
+import { useRouter } from "next/router";
+import { ProjectData } from "../../clients/project_service";
+import { FindUserByIdResponse, findUserById } from "../../clients/user_service";
+import youtube from "../../public/Project/youtube.png";
+import github from "../../public/Project/github.png";
+import website from "../../public/Project/web.png";
+import YouTube, { YouTubeProps } from "react-youtube";
+import dynamic from "next/dynamic";
 import "@uiw/react-markdown-preview/markdown.css";
+import Link from "next/link";
 import { Button, CircularProgress, Skeleton } from "@mui/material";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+import Stack from "@mui/material/Stack";
 
-const MarkdownPreview = dynamic(
-    () => import("@uiw/react-markdown-preview"),
-    { ssr: false }
-);
+const MarkdownPreview = dynamic(() => import("@uiw/react-markdown-preview"), {
+  ssr: false,
+});
 
 export default function ProjectPage() {
-    type UserNameId = {
-        name: string,
-        id: string
-    };
+  type UserNameId = {
+    name: string;
+    id: string;
+  };
 
-    const [project, set_project] = useState<ProjectData | null>(null);
-    const [members, set_members] = useState<Array<UserNameId> | null>(null);
-    const router = useRouter();
-    const { id: project_id } = router.query;
+  const [project, set_project] = useState<ProjectData | null>(null);
+  const [members, set_members] = useState<Array<UserNameId> | null>(null);
+  const router = useRouter();
+  const { id: project_id } = router.query;
 
+  useEffect(() => {
+    const run = async () => {
+      const final: UserNameId[] = [];
 
-    useEffect(() => {
-        const run = async () => {
-            const final: UserNameId[] = [];
+      if (typeof project_id === "string") {
+        const res = await find_project_by_project_id(project_id);
+        if (res.project !== null) {
+          set_project((_) => res.project);
+          const members_id = res.project?.members!;
+          const md_description = res.project?.description!;
+          if (members_id !== undefined) {
+            for (const id of members_id) {
+              const userNameId: UserNameId = {
+                name: "",
+                id: "",
+              };
 
-            if (typeof project_id === "string") {
-                const res = await find_project_by_project_id(project_id);
-                if (res.project !== null) {
-                    set_project(_ => res.project);
-                    const members_id = res.project?.members!;
-                    const md_description = res.project?.description!;
-                    if (members_id !== undefined) {
-                        for (const id of members_id) {
-                            const userNameId: UserNameId = {
-                                name: "",
-                                id: ""
-                            }
-
-                            const response = await findUserById(id);
-                            if (response.user != null) {
-                                userNameId.id = response.user.user_id;
-                                userNameId.name = response.user.name;
-                                final.push(userNameId);
-                            }
-                        }
-                        set_members(_ => final);
-                    }
-                }
+              const response = await findUserById(id);
+              if (response.user != null) {
+                userNameId.id = response.user.user_id;
+                userNameId.name = response.user.name;
+                final.push(userNameId);
+              }
             }
-
-        };
-        run();
-    }, [project_id]);
-
-    const onPlayerReady: YouTubeProps['onReady'] = (event) => {
-        // access to player in all event handlers via event.target
-        event.target.pauseVideo();
-    }
-
-    const opts: YouTubeProps['opts'] = {
-        height: '100%',
-        width: '100%',
-        playerVars: {
-            // https://developers.google.com/youtube/player_parameters
-            autoplay: 0,
-        },
-    };
-
-    var member_links = [];
-    if (members !== null) {
-        for (var i = 0; i < members.length; i++) {
-            const item = members[i];
-            member_links[i] = (
-                <Button className={style.member_item}
-                    variant="outlined"
-                    color="info"
-                    onClick={_ => {
-                        router.push(`/profiles/${item.id}`)
-                    }}>{item.name.toString()}</Button>);
+            set_members((_) => final);
+          }
         }
+      }
+    };
+    run();
+  }, [project_id]);
+
+  const onPlayerReady: YouTubeProps["onReady"] = (event) => {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  };
+
+  const opts: YouTubeProps["opts"] = {
+    height: "100%",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 0,
+    },
+  };
+
+  var member_links = [];
+  if (members !== null) {
+    for (var i = 0; i < members.length; i++) {
+      const item = members[i];
+      member_links[i] = (
+        <Button
+          className={style.member_item}
+          variant="outlined"
+          color="info"
+          onClick={(_) => {
+            router.push(`/profiles/${item.id}`);
+          }}
+        >
+          {item.name.toString()}
+        </Button>
+      );
     }
+  }
 
+  return (
+    <div className={style.page_container}>
+      <Navbar isLoading={false} />
 
-    return (
-        <div className={style.page_container}>
-
-            <Navbar isLoading={false} />
-
-            <div className={style.content_container}>
-
-                <div className={style.details_container}>
-                    <h1 className={style.project_name}>{project?.name!}</h1>
-                    <h4 className={style.project_short_description}>{project?.short_description!}</h4>
-                    <div className={style.member_list}>
-
-                        <div>{member_links}</div>
-                    </div>
-                </div>
-                <div className={style.upper_container}>
-                    <div className={style.left_side}>
-                        <Image src={project?.poster_image.base64!} layout="fill" objectFit="contain" alt="Project Poster" />
-                    </div>
-                    <div className={style.right_side}>
-                        <YouTube className={style.youtube_video} videoId={project?.youtube_link.substring(32, 43)!} opts={opts} onReady={onPlayerReady} />
-                    </div>
-                </div>
-                <div className={style.lower_container}>
-                    <MarkdownPreview source={project?.description!} warpperElement={{ "data-color-mode": "light" }} />
-                    <div className={style.hyperlinks}>
-                        <a href={project?.youtube_link!} target="_blank" rel="noreferrer"><div className={style.image_holder}><Image src={youtube} width={40} height={40} alt="Project Poster" /></div></a>
-                        <a href={project?.github_link!} target="_blank" rel="noreferrer"><div className={style.image_holder}><Image src={github} width={40} height={40} alt="Project Poster" /></div></a>
-                        <a href={project?.youtube_link!} target="_blank" rel="noreferrer"><div className={style.image_holder}><Image src={website} width={40} height={40} alt="Project Poster" /></div></a>
-                    </div>
-                </div>
-            </div>
-
-            <Footer />
+      <div className={style.content_container}>
+        <div className={style.details_container}>
+          <h1 className={style.project_name}>{project?.name!}</h1>
+          <h4 className={style.project_short_description}>
+            {project?.short_description!}
+          </h4>
+          <div className={style.member_list}>
+            <div>{member_links}</div>
+          </div>
         </div>
+        <div className={style.upper_container}>
+          <div className={style.left_side}>
+            <Image
+              src={project?.poster_image.base64!}
+              layout="fill"
+              objectFit="contain"
+              alt="Project Poster"
+            />
+          </div>
+          <div className={style.right_side}>
+            <YouTube
+              className={style.youtube_video}
+              videoId={project?.youtube_link.substring(32, 43)!}
+              opts={opts}
+              onReady={onPlayerReady}
+            />
+          </div>
+        </div>
+        <div className={style.lower_container}>
+          <MarkdownPreview
+            source={project?.description!}
+            warpperElement={{ "data-color-mode": "light" }}
+          />
+          <div className={style.hyperlinks}>
+            <a href={project?.youtube_link!} target="_blank" rel="noreferrer">
+              <div className={style.image_holder}>
+                <Image
+                  src={youtube}
+                  width={40}
+                  height={40}
+                  alt="Project Poster"
+                />
+              </div>
+            </a>
+            <a href={project?.github_link!} target="_blank" rel="noreferrer">
+              <div className={style.image_holder}>
+                <Image
+                  src={github}
+                  width={40}
+                  height={40}
+                  alt="Project Poster"
+                />
+              </div>
+            </a>
+            <a href={project?.youtube_link!} target="_blank" rel="noreferrer">
+              <div className={style.image_holder}>
+                <Image
+                  src={website}
+                  width={40}
+                  height={40}
+                  alt="Project Poster"
+                />
+              </div>
+            </a>
+          </div>
+          <div>
+            <div className={style.commentBar_input}>
+              <div>
+                <Box
+                  sx={{
+                    paddingTop: 5,
+                    width: 900,
+                    height: 150,
+                  }}
+                >
+                  <TextField fullWidth label="Comment" id="Comment" />
+                </Box>
+              </div>
+              <div>
+                <Button
+                  variant="contained"
+                  endIcon={<SendIcon />}
+                  sx={{
+                    marginTop: 5,
+                    marginLeft: 5,
+                    paddingTop: 2,
+                    paddingBottom: 2,
+                  }}
+                >
+                  Upload
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
-
-    )
+      <Footer />
+    </div>
+  );
 }
