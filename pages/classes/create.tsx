2568cdc96bdc15b4283177b633e6b0784b7108be
Bumskee <fadhlanrazan@gmail.com>
2022-12-24 21:07:05 +0700

    connected the function for searching classes based on lecturer id in the frontend with the backend

--- pages/classes/create.tsx
+++ pages/classes/create.tsx
@@ -7,6 +7,7 @@ import { ClassData, create_class } from "../../clients/class_service";
 import { getCurrentUserId } from "../../clients/azure_client";
 import { useMsal } from '@azure/msal-react';
 import { findUserByMsftProvider } from "../../clients/user_service";
+import { useRouter } from "next/router";
 
 export default function Classes() {
 
@@ -17,6 +18,8 @@ export default function Classes() {
     const [classCode, setClassCode] = useState<string>("");
     const [courseCode, setCourseCode] = useState<string>("");
     const [user_id, set_user_id] = useState<string>();
+    const router = useRouter();
+    const [isLoading, set_is_loading] = useState<boolean>(false);
 
     useEffect(() => {
         const run = async () => {
@@ -34,57 +37,58 @@ export default function Classes() {
 
     return (
         <>
-        <Navbar isLoading={false}/>
+        <Navbar isLoading={isLoading}/>
         <div className={style.container}>
             <h1> Create Class </h1>
             <div>
-            <TextField label="name" variant="outlined" margin="normal" fullWidth onChange={e => {
-                        const value = e.target.value;
-                        setName(value);
-                        console.log(value)
-                    }}/>
+                <TextField label="name" variant="outlined" margin="normal" fullWidth onChange={e => {
+                            const value = e.target.value;
+                            setName(value);
+                            console.log(value)
+                        }}/>
 
-            <TextField label="semester" variant="outlined" margin="normal" fullWidth onChange={e => {
-                        const value = e.target.value;
-                        setSemester(value);
-                        console.log(value)
-                    }}/>
+                <TextField label="semester" variant="outlined" margin="normal" fullWidth onChange={e => {
+                            const value = e.target.value;
+                            setSemester(value);
+                            console.log(value)
+                        }}/>
 
-            <TextField label="year" variant="outlined" margin="normal" fullWidth onChange={e => {
-                        const value = e.target.value;
-                        setYear(value);
-                        console.log(value)
-                    }}/>
+                <TextField label="year" variant="outlined" margin="normal" fullWidth onChange={e => {
+                            const value = e.target.value;
+                            setYear(value);
+                            console.log(value)
+                        }}/>
 
-            <TextField label="classCode" variant="outlined" margin="normal" fullWidth onChange={e => {
-                        const value = e.target.value;
-                        setClassCode(value);
-                        console.log(value)
-                    }}/>
+                <TextField label="classCode" variant="outlined" margin="normal" fullWidth onChange={e => {
+                            const value = e.target.value;
+                            setClassCode(value);
+                            console.log(value)
+                        }}/>
 
-            <TextField label="courseCode" variant="outlined" margin="normal" fullWidth onChange={e => {
-                        const value = e.target.value;
-                        setCourseCode(value);
-                        console.log(value)
-                    }}/>
+                <TextField label="courseCode" variant="outlined" margin="normal" fullWidth onChange={e => {
+                            const value = e.target.value;
+                            setCourseCode(value);
+                            console.log(value)
+                        }}/>
 
-            <Button  onClick={async (_) => {
-                if(user_id !== undefined) {
-                const classData: ClassData = {
-                    "class_id": "",
-                    "lecturer_id": user_id,
-                    "name": name,
-                    "semester": semester,
-                    "year": year,
-                    "class_code": classCode,
-                    "course_code": courseCode,
-                    "projects": []
-                }
-                const res = await create_class(classData)}}
-            }>
-                
-                    
-                    Submit</Button>
+                <Button  onClick={async (_) => {
+                    set_is_loading(true);
+                    if(user_id !== undefined) {
+                    const classData: ClassData = {
+                        "class_id": "",
+                        "lecturer_id": user_id,
+                        "name": name,
+                        "semester": semester,
+                        "year": year,
+                        "class_code": classCode,
+                        "course_code": courseCode,
+                        "projects": []
+                    }
+                    const res = await create_class(classData);
+                    router.push("/profile");
+                    set_is_loading(false);}}}>
+                    Submit
+                </Button>
             </div>
         </div>
 
